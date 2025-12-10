a51ef1d61b1fb6a8d6d9c9b6c7534b818dd3d684
iheartmywife <kylejchermak@gmail.com>
2025-12-10 09:40:13 -0800

    added wasd movement to player, began astroid class

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import pygame
 from logger import log_state
-from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_TURN_SPEED, PLAYER_SPEED
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    number = 5
-    while number > 0:
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-            return
+                return
         screen.fill("black")
+        for player in updatable:
+            player.update(dt)
+        for player in drawable:
+            player.draw(screen)
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
